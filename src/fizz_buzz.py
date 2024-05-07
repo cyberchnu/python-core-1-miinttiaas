3ceb6c6bbd4ad82ac32d0710d9ccb0102e575ce6
Anastasia <synyshyna.anastasiia@chnu.edu.ua>
2024-05-07 13:36:42 +0300

    done second task

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,14 @@
 def fizz_buzz(param):
   #Type your code here
-  return
+  if param % 3 == 0 and param % 5 == 0:
+    return "FizzBuzz"
+  if param % 3 == 0:
+    return "Fizz"
+  if param % 5 == 0:
+    return "Buzz"
+
+# print(fizz_buzz(3))
+# print(fizz_buzz(5))
+# print(fizz_buzz(7))
+# print(fizz_buzz(15))
+
