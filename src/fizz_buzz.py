09ba90a2f02ca80735aad2a1b972fd0393066371
Olexandr_Hramatovych <hramatovych.oleksandr@chnu.edu.ua>
2024-05-07 15:23:50 +0300

    зроблено 8 завдання

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,5 +1,5 @@
 def fizz_buzz(param):
-  #Type your code here
+
   if param % 3 == 0 and param % 5 == 0:
     return "FizzBuzz"
   if param % 3 == 0:
@@ -7,8 +7,5 @@ def fizz_buzz(param):
   if param % 5 == 0:
     return "Buzz"
 
-# print(fizz_buzz(3))
-# print(fizz_buzz(5))
-# print(fizz_buzz(7))
-# print(fizz_buzz(15))
+
 
