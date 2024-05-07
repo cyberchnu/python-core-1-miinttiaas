5fd5a829d18518c18ec2e9efdc2427a1fd13ec04
Anastasia <synyshyna.anastasiia@chnu.edu.ua>
2024-05-07 14:15:00 +0300

    зроблено сьоме завдання

--- src/makes10.py
+++ src/makes10.py
@@ -1,3 +1,9 @@
 def makes10(a, b):
-  # Type your code
-  return 
+  if a == 10 or b == 10 or a + b == 10:
+    return True
+  else:
+    return False
+# print(makes10(10,10))
+# print(makes10(10,9))
+# print(makes10(9,1))
+# print(makes10(9,0))
