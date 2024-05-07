4f1290315cc183bcd9141e131f9afa6b314d2bd7
Anastasia <synyshyna.anastasiia@chnu.edu.ua>
2024-05-07 14:02:46 +0300

    зроблено п'яте завдання

--- src/both.py
+++ src/both.py
@@ -1,3 +1,9 @@
 def both(number1, number2):
-  # Type your code
-  return
+  if number1 > 0 and number2 > 0 or number1 < 0 and number2 < 0 or number1 == 0 and number2 == 0:
+    return True
+  else:
+    return False
+
+# print(both(3,4))
+# print(both(0,0))
+# print(both(-1,0))
