75fba173f53d90bca61706dead99a77fc9e34c65
Anastasia <synyshyna.anastasiia@chnu.edu.ua>
2024-05-11 08:01:16 +0300

    зроблено дев'яте  завдання

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,9 @@
+
 def sum_even_nums_in_range(start, stop):
-  # Type your code
-  return
+  list_of_even_numbers = []
+  for n in range(start, stop+1):
+    if n % 2 == 0:
+      list_of_even_numbers.append(n)
+      print(n)
+  return sum(list_of_even_numbers)
+
